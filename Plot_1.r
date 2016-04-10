47fbd87e3645f80488dc6d026c01bd23bc3975e0
run93 <run93@foxmail.com>
2016-04-09 23:03:19 -0400

    Revert "Apr-9th, 2016"
    This reverts commit d30850a1780ece37700c3f7398a426a77b10ad18.

--- Plot_1.r
+++
@@ -1,8 +0,0 @@
-setwd("C:/Users/Xiang/Desktop/R-Coursera/Plot/Week4")
-NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
-
-## Q1
-
-total_emissions<-aggregate(Emissions ~ year,NEI, sum)
-plot_1<-plot(total_emissions, type="l", main = "Emissions", ylab = "Emissions [Tons]", xlab= "Year")
--- Plot_2.r
+++
@@ -1,9 +0,0 @@
-setwd("C:/Users/Xiang/Desktop/R-Coursera/Plot/Week4")
-NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
-
-## Q2
-
-baltimore_NEI<-NEI[NEI$fips=="24510",]
-baltimore_emissions<-aggregate(Emissions ~ year,baltimore_NEI, sum)
-plot_2<-plot(baltimore_emissions, type="l", main = "Emissions", ylab = "Emissions [Tons]", xlab= "Year")
